a66878254ab117b30bd77ee2cc8ab46706114f2b
NTD4503 <nguyendat12501@gmail.com>
2022-05-15 17:41:38 +0700

    lab5

---
+++ lab05/OtherProjects/hust/soict/hedspi/lab01/FirstDialog.java
@@ -0,0 +1,10 @@
+package OtherProjects.hust.soict.hedspi.lab01;
+
+import javax.swing.*;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Xin chào");
+        System.exit(0);
+    }
+}
